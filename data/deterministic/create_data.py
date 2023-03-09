5d0a5815ded88cb0a18b01d225cfed92bfe1069b
LukasCironis <51169823+LukasCironis@users.noreply.github.com>
2023-03-09 14:16:32 +0000

    determinisitic simplify

--- data/deterministic/create_data.py
+++ data/deterministic/create_data.py
@@ -5,9 +5,10 @@ import matplotlib.pyplot as plt
 from gan.output import Output, OutputType, Normalization
 
 
-def trig(type, freq, shift, amp=1, 
-         n=1000, xmin=-500, xmax=500):
+def trig(type, freq, amp=1, 
+         n=1005, xmin=-500):
     
+    xmax = xmin + 2
     by = (xmax - xmin)/n
     x = np.arange(xmin, xmax, by)
     if type == "sin":
@@ -17,13 +18,23 @@ def trig(type, freq, shift, amp=1,
     else:
         raise NotImplementedError("'type' is only sin or cos")
     
-    df=pd.DataFrame(np.array([x, y]).T, columns=["x", "y"])
+    df=pd.DataFrame(np.array([y[5:]]).T, columns=["y"])
     df.loc[:,"type"] = type
     df.loc[:,"freq"] = freq
-    df.loc[:,"shift"] = shift
-    #df.loc[:,"amp"] = amp
-    df.loc[:,"xmin"] = xmin
-    df.loc[:,"xmax"] = xmax
+    # df.loc[:,"shift"] = shift
+    # df.loc[:,"xmin"] = x[5]
+    
+    df.loc[:,"ylag1"] = y[4]
+    df.loc[:,"ylag2"] = y[3]
+    df.loc[:,"ylag3"] = y[2]
+    df.loc[:,"ylag4"] = y[1]
+    df.loc[:,"ylag5"] = y[0]
+    
+    # df.loc[:,"xlag1"] = x[4]
+    # df.loc[:,"xlag2"] = x[3]
+    # df.loc[:,"xlag3"] = x[2]
+    # df.loc[:,"xlag4"] = x[1]
+    # df.loc[:,"xlag5"] = x[0]
 
     return df
     
@@ -32,20 +43,18 @@ if __name__ == "__main__":
     
     fig, axes = plt.subplots(1,1)
 
-    for i in range(3):
+    for i in range(5):
            
         pars = {
             "type" : "cos",
             "freq" : [0.5, 1, 2, 5][i],
-            "shift": [0.5, 1, 2, 5][i],
             "amp"  : 1,  
             "xmin": -5, 
-            "xmax": 5, 
-            "n" : 50
+            "n" : 105
         }
         
         df_test = trig(**pars)
-        df_test.plot(x="x", y="y", ax=axes)
+        df_test.y.plot(ax=axes)
     
     plt.show()
     
@@ -54,7 +63,8 @@ if __name__ == "__main__":
     atribute_list = []
 
     X_MIN = -6
-    X_MAX = 6
+    X_MINMAX = 4
+    X_MAX = X_MINMAX + 2
     FREQ_MAX = 6
     FREQ_MIN = 0
     SHIFT_MAX = 7
@@ -63,34 +73,34 @@ if __name__ == "__main__":
     for _ in range(100):
         type = np.random.choice(["cos", "sin"])
         freq = 6*np.random.random_sample(1)[0]
-        shift = 7*np.random.random_sample(1)[0]
-        xmin = np.random.uniform(X_MIN, 2)
-        xmax = np.random.uniform(3, X_MAX)
-        tmp = trig(type, freq, shift, xmin=xmin, xmax=xmax, n=128)
+        #shift = 7*np.random.random_sample(1)[0]
+        xmin = np.random.uniform(X_MIN, X_MINMAX)
+        xmax = xmin + 2
+        tmp = trig(type, freq, xmin=xmin, n=100+5)
         
         #normalize        
         freq = (freq - FREQ_MIN)/(FREQ_MAX - FREQ_MIN)
-        shift = (shift - SHIFT_MIN)/(SHIFT_MAX - SHIFT_MIN)
-        xmin = 2*(xmin - X_MIN)/(X_MAX - X_MIN) - 1
-        xmax = 2*(xmax - X_MIN)/(X_MAX - X_MIN) - 1
+        #shift = (shift - SHIFT_MIN)/(SHIFT_MAX - SHIFT_MIN)
+        #xmin = 2*(xmin - X_MIN)/(X_MINMAX - X_MIN) - 1
+        #xmax = 2*(xmax - X_MIN)/(X_MINMAX - X_MIN) - 1
 
-        tmp.loc[:, "x"] = 2*(tmp.loc[:, "x"]-X_MIN)/(X_MAX-X_MIN) - 1
+        #tmp.loc[:, "x"] = 2*(tmp.loc[:, "x"]-X_MIN)/(X_MAX-X_MIN) - 1
         tmp.loc[:, "freq"] = freq
-        tmp.loc[:, "shift"] = shift
-        tmp.loc[:, "xmin"] = xmin
-        tmp.loc[:, "xmax"] = xmax
+        #tmp.loc[:, "shift"] = shift
+        #tmp.loc[:, "xmin"] = xmin
 
+        #tmp.iloc[:,-5:] = 2*(tmp.iloc[:,-5:]-X_MIN)/(X_MAX-X_MIN) - 1
+        
         df_list.append(tmp)
         atribute_list.append([
             1 if type=="cos" else 0,
             1 if type=="sin" else 0, 
             freq, 
-            shift, 
-            xmin, 
-            xmax
-            ])
+            #shift, 
+            #xmin, 
+            ] + list(tmp.iloc[0,-5:]))
         
-        feature_list.append(tmp[["x", "y"]].values)
+        feature_list.append(tmp[["y"]].values)
     
     df = pd.concat(df_list).reset_index(drop=True) 
     df.type.value_counts()
@@ -106,19 +116,20 @@ if __name__ == "__main__":
     atribute_np.max()
     
     data_gen_flag = np.ones((features_np.shape[0],features_np.shape[1]))
-
+    data_gen_flag.shape
+    
     data_feature_output = [
         #Numeric
         Output(type_=OutputType.CONTINUOUS, dim=1, normalization=Normalization.MINUSONE_ONE, is_gen_flag=False),
-        Output(type_=OutputType.CONTINUOUS, dim=1, normalization=Normalization.MINUSONE_ONE, is_gen_flag=False),
+        #Output(type_=OutputType.CONTINUOUS, dim=1, normalization=Normalization.MINUSONE_ONE, is_gen_flag=False),
     ]
         
     data_attribute_output = [
         Output(type_=OutputType.DISCRETE, dim=2, normalization=None, is_gen_flag=False),
         Output(type_=OutputType.CONTINUOUS, dim=1, normalization=Normalization.ZERO_ONE, is_gen_flag=False),
-        Output(type_=OutputType.CONTINUOUS, dim=1, normalization=Normalization.ZERO_ONE, is_gen_flag=False),
-        Output(type_=OutputType.CONTINUOUS, dim=1, normalization=Normalization.MINUSONE_ONE, is_gen_flag=False),
-        Output(type_=OutputType.CONTINUOUS, dim=1, normalization=Normalization.MINUSONE_ONE, is_gen_flag=False),
+        Output(type_=OutputType.CONTINUOUS, dim=5, normalization=Normalization.MINUSONE_ONE, is_gen_flag=False),
+        #Output(type_=OutputType.CONTINUOUS, dim=1, normalization=Normalization.MINUSONE_ONE, is_gen_flag=False),
+        #Output(type_=OutputType.CONTINUOUS, dim=1, normalization=Normalization.MINUSONE_ONE, is_gen_flag=False),
     ]
 
     with open('data_feature_output.pkl', 'wb') as f:
@@ -135,7 +146,7 @@ if __name__ == "__main__":
     
     #visualize
     for i in range(6,10):
-        plt.plot(features_np[i][:,0], features_np[i][:,1])
+        plt.plot(features_np[i])
     
     plt.show()
     
--- data/deterministic/visualise.py
+++ data/deterministic/visualise.py
@@ -37,7 +37,8 @@ def trig(type, freq, shift, amp=1,
 print("Current dir:", os.getcwd())
 
 real_npz = np.load("data_train.npz")
-synth_npz = np.load(os.path.join("..","..", "test\\backup_deterministic_30000\\test\\generated_samples\\epoch_id-9999\\generated_data_train.npz"))
+#synth_npz = np.load(os.path.join("..","..", "test\\backup_deterministic_30000\\test\\generated_samples\\epoch_id-29999\\generated_data_train.npz"))
+synth_npz = np.load(os.path.join("..","..", "test\\backup_deterministic_30000\\fixed_atribute_e30000\\epoch_id-29999\\generated_data_train.npz"))
 
 real_feature_np = real_npz["data_feature"]
 real_atribute_np = real_npz["data_attribute"]
@@ -46,20 +47,6 @@ real_flag = real_npz["data_gen_flag"]
 synth_feature_np = synth_npz["data_feature"]
 synth_atribute_np = synth_npz["data_attribute"]
 synth_flag = synth_npz["data_gen_flag"]
-
-
-pd.DataFrame(real_atribute_np, columns = [
-    "type_glag1",
-    "type_glag2",
-    "freq",
-    "shift",
-    "xmin",
-    "xmax"
-    ]
-    )
-
-
-
 
 # for i in range(20):
 #     print(synth_atribute_np[i])
@@ -81,7 +68,7 @@ SHIFT_MAX = 7
 SHIFT_MIN = 0
     
 for i in range(20):
-
+    print(i)
     freq_n = synth_atribute_np[i][2]
     shift_n = synth_atribute_np[i][3]
     xmin_n = synth_atribute_np[i][4]
@@ -132,4 +119,52 @@ for i in range(20):
     ax[1,1].title.set_text('raw time mapping')
     
     plt.show()
+    
+# Fix atribute generate cloud
+
+i=0
+freq_n = synth_atribute_np[i][2]
+shift_n = synth_atribute_np[i][3]
+xmin_n = synth_atribute_np[i][4]
+xmax_n = synth_atribute_np[i][5]
+x_n = synth_feature_np[i,:,0]
+
+freq = freq_n*(FREQ_MAX - FREQ_MIN) + FREQ_MIN
+shift = shift_n*(SHIFT_MAX - SHIFT_MIN) + SHIFT_MIN
+xmin = (xmin_n+1)*((X_MAX - X_MIN)/2) + X_MIN
+xmax = (xmax_n+1)*((X_MAX - X_MIN)/2) + X_MIN
+x = (x_n+1)*((X_MAX - X_MIN)/2) + X_MIN
+y = synth_feature_np[i,:,1]
+
+df_og_cos = trig("cos", freq, shift, xmin=xmin, xmax=xmax, n=len(x))
+df_og_sin = trig("sin", freq, shift, xmin=xmin, xmax=xmax, n=len(x))
+
+df_og = df_og_cos.copy()
+df_og.loc[:,"y"] = df_og_cos.y*synth_atribute_np[i][0] + df_og_sin.y*synth_atribute_np[i][1] 
+
+plt.plot(df_og.x, df_og.y, label="og", color="red")  
+for i in range(200):
+    x_n = synth_feature_np[i,:,0]
+    x = (x_n+1)*((X_MAX - X_MIN)/2) + X_MIN
+    y = synth_feature_np[i,:,1]
+    
+    df_synth_renormed = pd.DataFrame(np.array([x,y]).T, columns = ["x", "y"])
+
+    xmin_tmp = df_synth_renormed.x.min()
+    xmax_tmp = df_synth_renormed.x.max()
+
+    x_synth_corrected = np.linspace(xmin_tmp, xmax_tmp, num=len(x))
+    df_synth_corrected = df_synth_renormed.copy()
+    df_synth_corrected.loc[:,"x"] = x_synth_corrected
+    
+    xmin_tmp = df_synth_renormed.x.min()
+    xmax_tmp = df_synth_renormed.x.max()
+
+    x_synth_corrected = np.linspace(xmin_tmp, xmax_tmp, num=len(x))
+    df_synth_corrected = df_synth_renormed.copy()
+    df_synth_corrected.loc[:,"x"] = x_synth_corrected
+    
+    plt.plot(df_synth_corrected.x, df_synth_corrected.y, label="synth", color="black", alpha=0.04)   
+  
+plt.show()
     
